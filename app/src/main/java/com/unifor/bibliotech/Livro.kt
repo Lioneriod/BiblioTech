0593455db6b3816eee8068eb564899764e8ff3c5
YoInsider <insensoscoffee17@proton.me>
2025-10-29 18:54:22 -0300

    primeiro commit aparecer livros

---
+++ app/src/main/java/com/unifor/bibliotech/Livro.kt
@@ -0,0 +1,8 @@
+package com.unifor.bibliotech
+
+data class Livro(
+    val status: Boolean,
+    val titulo: String,
+    val autor: String,
+    val anoPub: String
+)
---
+++ app/src/main/java/com/unifor/bibliotech/LivroAdapter.kt
@@ -0,0 +1,25 @@
+package com.unifor.bibliotech
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+
+class LivroAdapter(private val listaLivros: List<Livro>): RecyclerView.Adapter<LivroAdapter.LivroViewHolder>() {
+
+    class LivroViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val clLivro: ConstraintLayout = itemView.findViewById(R.id.cardLivro)
+        val tvStatus: TextView = itemView.findViewById(R.id.tvLivroStatus)
+        val tvTitulo: TextView = itemView.findViewById(R.id.tvLivroTitulo)
+        val tvAutor: TextView = itemView.findViewById(R.id.tvLivroAutor)
+        val tvAnoPub: TextView = itemView.findViewById(R.id.tvLivroAno)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LivroViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_livro, parent, false)
+        return LivroViewHolder(view)
+    }
+}
