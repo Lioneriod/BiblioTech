622f0910d812829c96028623d5a8b189e4e5443a
YoInsider <insensoscoffee17@proton.me>
2025-11-03 18:51:38 -0300

    first edit of firebase

--- app/src/main/java/com/unifor/bibliotech/ActivityCadastro.kt
+++ app/src/main/java/com/unifor/bibliotech/ActivityCadastro.kt
@@ -8,21 +8,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat.enableEdgeToEdge
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ActivityCadastro : AppCompatActivity() {
+    lateinit var fb: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.telaCadastro)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+       fb = Firebase.firestore
 
         val linkLogin: TextView = findViewById(R.id.btnLogin)
 
         linkLogin.setOnClickListener {
+
+            fb.collection("usuario")
+                .add(mapOf(
+                    "nome" to "variavelNome",
+                    "nome" to "variavelNome"
+                ))
+
             finish()
         }
     }
